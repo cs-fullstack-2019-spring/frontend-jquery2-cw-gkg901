99ebf3623bff33c64d1251fee9cc22e7672f0b5a
gkg901 <gerren.gregory@gmail.com>
2019-02-08 12:05:40 -0600

    jQUERY is saving my life

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 
 /* Give everything with the class 'hello' a white background when you click on one of them*/
 
+//SELECTS HELLO CLASS AND CHANGES BACKGROUND COLOR USING SHORTHAND CSS
 $('.hello').on('click', function () {
     $(this).css("background", "red")
 });
@@ -13,6 +14,7 @@ $('.hello').on('click', function () {
 /* When you mouse over the "Selectors Exercise" element, change the height&width to 20px.
  When you mouse off of it change it back */
 
+//SELECTS H1 ELEMENT AND ADDS, THEN REMOVES A 20PX HEIGHT AND WIDTH CLASS
 $("h1").on("mouseover", function () {
     $(this).addClass("changeSize")
 }).on("mouseout", function () {
@@ -21,6 +23,8 @@ $("h1").on("mouseover", function () {
 
 
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
+
+//SELECTS FIRST ELEMENT IN HELLO CLASS AND APPENDS A "." ON EVERY CLICK
 $(".hello:first").click(function(){
     $(this).append(".");
 });
