662d096337cb430893e7ea5009f5dca581d9cfc4
gkg901 <gerren.gregory@gmail.com>
2019-02-08 11:53:27 -0600

    jQuery is very powerful -Kevin

--- js.js
+++ js.js
@@ -5,8 +5,22 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 
 /* Give everything with the class 'hello' a white background when you click on one of them*/
 
+$('.hello').on('click', function () {
+    $(this).css("background", "red")
+});
 
-/* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
+
+/* When you mouse over the "Selectors Exercise" element, change the height&width to 20px.
+ When you mouse off of it change it back */
+
+$("h1").on("mouseover", function () {
+    $(this).addClass("changeSize")
+}).on("mouseout", function () {
+    $(this).removeClass("changeSize")
+});
 
 
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
+$(".hello:first").click(function(){
+    $(this).append(".");
+});
